39245a1c32a5c889cd56806fae7b732610db0e3c
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2019-01-29 11:57:33 +0100

    Prueba

--- app/src/main/java/com/example/gabrielcuenca/spaceinvaders/ChildGameViewActivity.java
+++ app/src/main/java/com/example/gabrielcuenca/spaceinvaders/ChildGameViewActivity.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 public class ChildGameViewActivity extends Activity
 {
+    
     // spaceInvadersView será la visualización del juego
     // También tendrá la lógica del juego
     // y responderá a los toques a la pantalla
